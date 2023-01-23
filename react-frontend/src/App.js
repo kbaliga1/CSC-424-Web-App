a127b0cce4b6afa7fbb601c47a193c7b616c9913
kbaliga1 <kbaliga@calpoly.edu>
2023-01-22 21:57:33 -0800

    First commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {Routes, Route, Link, NavLink} from "react-router-dom";
+import {Home} from "./Home";
+import {Landing} from "./Landing";
+import { ProtectedRoute } from "./utils/ProtectedRoute";
+import {fakeAuth} from "./utils/FakeAuth"
+import { useAuth } from "./context/AuthProvider";
+import { AuthProvider } from "./context/AuthProvider";
+
+export const AuthContext = React.createContext(null);  // we will use this in other components
+
+const App = () => {
+
+    return (
+        <AuthProvider>
+            <Navigation />
+
+            <h1>React Router</h1>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route
+                    path="landing"
+                    element={
+                        <ProtectedRoute>
+                            <Landing />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="home" element={ <Home />} />
+                <Route path="*" element={<p>There's nothing here: 404!</p>} />
+            </Routes>
+        </AuthProvider>
+     );
+
+};
+
+const Navigation = () => {
+    const { value } = useAuth();
+    return(
+    <nav>
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/landing">Landing</NavLink>
+        {value.token && (
+            <button type="button" onClick={value.onLogout}>
+                Sign Out
+            </button>
+        )}
+    </nav>
+    );
+};
+
+
+export default App;
